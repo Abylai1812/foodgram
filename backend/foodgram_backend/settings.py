e1f0d6784eaceb7c5be68d816cc10b8fe7b1e533
Abylai1812 <abylaikhhann@mail.ru>
2026-02-15 09:39:17 +0500

    испарвление админ части

--- backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/settings.py
@@ -17,7 +17,7 @@ load_dotenv(BASE_DIR.parent / '.env')
 
 SECRET_KEY = os.getenv('SECRET_KEY', 'dev-secret-key')
 
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
 
